e9b4ed191bef2e4faa736ad821831499bf0f3a91
Alvaro Separovich <alvasepro@gmail.com~>
2023-12-14 23:13:13 -0300

    updates

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const fs = require('fs')
 const cheerio = require('cheerio')
 const readline = require('readline')
 
+function removerAcentos(str) {
+  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+function removePontos(str){
+  return str.replace(/[.,\/#!$%\^&\*;:{}=`~()"\s]/g, '');
+}
 function toFileName(inputString) {
   // Convert to lowercase and replace spaces with underscores
-  let transformedString = inputString.toLowerCase().replace(/ /g, '_');
+  let transformedString
+  transformedString = inputString.toLowerCase().replace(/ /g, '_');
+  transformedString = removerAcentos(transformedString)
+  transformedString = removePontos(transformedString)
+
   return transformedString;
 }
+
 
 // Create a readline interface with the CSV file as input
 const rl = readline.createInterface({
@@ -20,9 +31,13 @@ let html = fs.readFileSync('convidados.html', 'utf-8')
 
 // Listen for the 'line' event on the readline interface
 rl.on('line', (line) => {
-  let values = line.split(",")
-  let convidado = values[0]
-  console.log(convidado)
+
+  let convidado
+  if (line.includes('"')){
+    convidado = line.split('"')[1]
+  } else {
+    convidado = line.split(",")[0]
+  }
   // Load the HTML content into Cheerio
   let $ = cheerio.load(html);
   
@@ -30,5 +45,7 @@ rl.on('line', (line) => {
   $('.convidado-nome').text(convidado);
   
   // Write the modified HTML back to the file
-  fs.writeFileSync(`convite/${toFileName(convidado)}.html`, $.html());
+  file = `convite/${toFileName(convidado)}.html`
+  fs.writeFileSync(file, $.html());
+  console.log(`https://casamento.separovi.ch/${file}`)
 })
